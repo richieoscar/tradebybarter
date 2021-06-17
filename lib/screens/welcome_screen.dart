4b16a12030a81fa0422ef49453b04af7eda7ef57
EunicePops <enablersolutions@gmail.com>
2021-06-17 12:50:11 +0100

    integration of splash screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Hero(
+                  tag: "logo",
+                  child: Image.asset("images/logo.png")),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+
+            ],
+          ),
+        ),
+      )
+    );
+
+  }
+
+
+}
