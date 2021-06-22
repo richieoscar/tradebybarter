8f5c5160def0d0a56b2c6b1066ffdc2ecac6fd5f
ogomegbunam <Okoriechristian001@gmail.com>
2021-06-22 13:13:41 +0100

    na
    kiki1wkq1

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:trade_by_barter/signUp.dart';
+<<<<<<< Updated upstream
 import 'screens/welcome_screen.dart';
+=======
+>>>>>>> Stashed changes
 
 void main() {
   runApp(MyApp());
@@ -12,8 +15,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< Updated upstream
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
+=======
+      title: 'Splash Screen',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Signup(),
+>>>>>>> Stashed changes
     );
   }
 }
