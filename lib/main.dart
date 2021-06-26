68eb65037b88b181ee4eb49fcb66c7a479312825
ogomegbunam <Okoriechristian001@gmail.com>
2021-06-26 16:10:32 +0100

    location screen updated
    location screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:trade_by_barter/screens/basedOnLocation.dart';
 import 'package:trade_by_barter/screens/registerScreen.dart';
 import 'screens/onboardScreen.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,34 +19,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   @override
- _SplashScreenState createState() => _SplashScreenState();
-
+  _SplashScreenState createState() => _SplashScreenState();
 }
-class _SplashScreenState extends State<SplashScreen>{
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     Timer(
-      Duration(
-        seconds: 5,
-      ),(){
-        Navigator.pushReplacement(context, MaterialPageRoute(
-            builder:(context) => OnBoardScreen(),
-        ));
-    }
-    );
+        Duration(
+          seconds: 5,
+        ), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Location(),
+          ));
+    });
     super.initState();
   }
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: Center(
-        child: Hero(
-          tag: "logo",
-          child: Image.asset("images/logo.png"))
-        )
-      );
-  }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Hero(tag: "logo", child: Image.asset("images/logo.png"))));
+  }
 }
