5948e375202e671db799995f7fa50a114a01aee9
EunicePops <enablersolutions@gmail.com>
2021-06-28 16:46:40 +0100

    Update onboardScreen.dart

--- lib/screens/onboardScreen.dart
+++ lib/screens/onboardScreen.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:trade_by_barter/navigation/navigation.dart';
 
 import '../constants.dart';
-///This is the stateful widget that the main application instantiates
-class OnBoardScreen extends StatefulWidget{
+
+class OnBoardScreen extends StatefulWidget {
   @override
   _OnBoardScreenState createState() => _OnBoardScreenState();
-
 }
 
 int _currentPage = 0;
@@ -14,215 +14,128 @@ int _currentPage = 0;
 final _controller = PageController(
   initialPage: 0,
 );
-///The list of pages on the PageView
-List<Widget> _pages = [
-  Column(
-    children: [
-      Expanded(child: Image.asset("images/onboardscreen-1.png")),
-      Text("Exchange your items for another", style: KPageViewTextStyle, textAlign: TextAlign.center),
-      SizedBox(
-        height: 20,
-      ),
-      Text("Bring your item and get",style: KPageViewTextStyle2, textAlign: TextAlign.center ),
-      SizedBox(
-        height: 20,
-      ),
-      Text("to trade with another trader",style: KPageViewTextStyle2, textAlign: TextAlign.center ),
-      SizedBox(
-          height: 20
-      ),
-      Container(
-        height: 40,
-        width: 100,
-        child: (
-            OutlinedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(KProceedColor),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.red, width: 20,),
-                    borderRadius: BorderRadius.circular(15.0)
-                )),
 
-              ),
+List<Widget> pageView(BuildContext context) {
+  List<Widget> pages = [
+    Column(
+      children: [
+        Container(
+          //constraints: BoxConstraints.expand(width: 300, height: 100),
+          child: Image.asset(
+            "images/onboardscreen-1.png",
 
-              onPressed: () {
-                print('Skip clicked');
-              },
-              child: Text("Skip",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20
-                ),
-              ),
-            )),
-      ),
-      SizedBox(
-        height: 60,
-      ),
-    ],
-  ),
-  Column(
-    children: [
-
-    /* ConstrainedBox(
-        constraints: BoxConstraints(
-            minHeight: 373,
-            minWidth: 353
-        ),
-        child: Expanded(
-          child: Container(
-            child: Image.asset("images/onboardscreen-3.png"),
           ),
         ),
-      ),*/
-      Expanded(child: Image.asset("images/onboardscreen-2.png")),
-      Text("Exchange your items for another",style: KPageViewTextStyle,textAlign: TextAlign.center ),
-      SizedBox(
-        height: 20,
-      ),
-      Text("Bring your item and get",style: KPageViewTextStyle2, textAlign: TextAlign.center ),
-      SizedBox(
-        height: 20,
-      ),
-      Text("to trade with another trader",style: KPageViewTextStyle2, textAlign: TextAlign.center ),
-      SizedBox(
-          height: 20
-      ),
-      Container(
-        height: 40,
-        width: 100,
-        child: (
-            OutlinedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(KProceedColor),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.red, width: 20,),
-                    borderRadius: BorderRadius.circular(15.0)
-                )),
-
-              ),
-
-              onPressed: () {
-                print('Skip clicked');
-              },
-              child: Text("Skip",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20
-                ),
-              ),
-            )),
-      ),
-      SizedBox(
-        height: 60,
-      ),
-    ],
-  ),
-  Column(
-    children: [
-      /*ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: 373,
-            minWidth: 353
-
-      ),
-        child:
-        Expanded(
-          child: Container(
-            child: Image.asset("images/onboardscreen-3.png"),
-          ),
+        Text("Exchange your items for another",
+            style: KPageViewTextStyle, textAlign: TextAlign.center),
+        SizedBox(
+          height: 20,
         ),
-      ),*/
-      Container(
-        constraints: BoxConstraints.expand(),
-        child: Image.asset("images/onboardscreen-3.png"),
-      ),
-      //Expanded(child: Image.asset("images/onboardscreen-3.png")),
-      Text("Exchange your items for another", style: KPageViewTextStyle,textAlign: TextAlign.center ),
-      SizedBox(
-        height: 20,
-      ),
-      Text("Bring your item and get",style: KPageViewTextStyle2, textAlign: TextAlign.center ),
-      SizedBox(
-        height: 20,
-      ),
-      Text("to trade with another trader",style: KPageViewTextStyle2, textAlign: TextAlign.center ),
-      SizedBox(
-        height: 20
-      ),
-      Container(
-        height: 40,
-        width: 200,
-        child: (
-            OutlinedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(KBrandColors),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.red, width: 20,),
-                    borderRadius: BorderRadius.circular(15.0)
-                )),
+        Text("Bring your item and get",
+            style: KPageViewTextStyle2, textAlign: TextAlign.center),
+        SizedBox(
+          height: 20,
+        ),
+        Text("to trade with another trader",
+            style: KPageViewTextStyle2, textAlign: TextAlign.center),
+        SizedBox(height: 20),
+      ],
+    ),
+    Column(
+      children: [
+        Image.asset("images/onboardscreen-2.png"),
+        Text("Exchange your items for another",
+            style: KPageViewTextStyle, textAlign: TextAlign.center),
+        SizedBox(
+          height: 20,
+        ),
+        Text("Bring your item and get",
+            style: KPageViewTextStyle2, textAlign: TextAlign.center),
+        SizedBox(
+          height: 20,
+        ),
+        Text("to trade with another trader",
+            style: KPageViewTextStyle2, textAlign: TextAlign.center),
+        SizedBox(height: 20),
+      ],
+    ),
+    Column(
+      children: [
+        Image.asset("images/onboardscreen-3.png"),
+        Text("Exchange your items for another",
+            style: KPageViewTextStyle, textAlign: TextAlign.center),
+        SizedBox(
+          height: 20,
+        ),
+        Text("Bring your item and get",
+            style: KPageViewTextStyle2, textAlign: TextAlign.center),
+        SizedBox(
+          height: 20,
+        ),
+        Text("to trade with another trader",
+            style: KPageViewTextStyle2, textAlign: TextAlign.center),
+        SizedBox(height: 20),
+      ],
+    ),
+  ];
+  return pages;
+}
 
-              ),
-
-              onPressed: () {
-                print('started clicked');
-              },
-              child: Text("Let's Get Started",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20
-                ),
-              ),
-            )),
-      ),
-      SizedBox(
-        height: 60,
-      ),
-    ],
-  ),
-];
-
-class _OnBoardScreenState extends State<OnBoardScreen>{
+class _OnBoardScreenState extends State<OnBoardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           Expanded(
-             child: PageView(
-              controller: _controller,
-              children: _pages,
-               onPageChanged: (index){
-                setState(() {
-                  _currentPage = index;
-                });
-               },
-              // scrollDirection: Axis.horizontal,
+            Expanded(
+              child: PageView(
+                controller: _controller,
+                children: pageView(context),
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+              ),
             ),
-
-           ),
             new DotsIndicator(
-              dotsCount: _pages.length,
+              dotsCount: pageView(context).length,
               position: _currentPage.toDouble(),
               decorator: DotsDecorator(
                 size: const Size.square(9.0),
                 activeSize: const Size(18.0, 9.0),
-                activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                activeShape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
               ),
             ),
-            SizedBox(
-              height: 100,
-            )
 
+            Container(
+              width: 200,
+              child: (OutlinedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(KfilterBorderColors ),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0))),
+                ),
+                onPressed: () {
+                  print('Proceed clicked');
+                  AppNavigator.navigateToWelcomeScreen(context);
+                },
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15),
+                ),
+              )),
+            ),
+            SizedBox(height: 200,)
           ],
         ),
       ),
-      
-
     );
   }
 }
