7d386826865ac84ee78887024e1e17af898c97d3
EunicePops <enablersolutions@gmail.com>
2021-07-04 23:06:28 +0100

    Edited the onboard screens

--- lib/screens/onboardScreen.dart
+++ lib/screens/onboardScreen.dart
@@ -17,68 +17,182 @@ final _controller = PageController(
 
 List<Widget> pageView(BuildContext context) {
   List<Widget> pages = [
-    Column(
-      children: [
-        Container(
-          //constraints: BoxConstraints.expand(width: 300, height: 100),
-          child: Image.asset(
-            "images/onboardscreen-1.png",
 
+    Container(
+      decoration: BoxDecoration(color: Colors.white),
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/onb1.png'),
+                  fit: BoxFit.cover)),
+          child: Column(
+            children: [
+
+              SizedBox(height: 320,),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text("Barter Your Unneeded Items Instantly.",
+                    style: KPageViewTextStyle, textAlign: TextAlign.center),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Swap your items and",
+                  style: KPageViewTextStyle2, textAlign: TextAlign.center),
+              SizedBox(
+                height: 20,
+              ),
+              Text("meet your real time needs",
+                  style: KPageViewTextStyle2, textAlign: TextAlign.center),
+              SizedBox(height: 100),
+              Container(
+                width: 100,
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  constraints: BoxConstraints.expand( height: 30),
+                  child: (OutlinedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(KProceedColor  ),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                    ),
+                    onPressed: () {
+                      print('skip clicked');
+                      AppNavigator.navigateToWelcomeScreen(context);
+                    },
+                    child: Text(
+                      "Skip",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15),
+                    ),
+                  )),
+                ),
+              ),
+            ],
           ),
         ),
-        SizedBox(height: 50,),
-        Text("Exchange your items for another",
-            style: KPageViewTextStyle, textAlign: TextAlign.center),
-        SizedBox(
-          height: 20,
-        ),
-        Text("Bring your item and get",
-            style: KPageViewTextStyle2, textAlign: TextAlign.center),
-        SizedBox(
-          height: 20,
-        ),
-        Text("to trade with another trader",
-            style: KPageViewTextStyle2, textAlign: TextAlign.center),
-        SizedBox(height: 20),
-      ],
+      ),
     ),
-    Column(
-      children: [
-        Image.asset("images/onboardscreen-2.png"),
+    Container(
+      decoration: BoxDecoration(color: Colors.white),
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
 
-        Text("Exchange your items for another",
-            style: KPageViewTextStyle, textAlign: TextAlign.center),
-        SizedBox(
-          height: 20,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/onb2.png'),
+                  fit: BoxFit.cover)),
+          child: Column(
+            children: [
+
+              SizedBox(height: 320,),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text("Get Equal Value of Your Items.",
+                    style: KPageViewTextStyle, textAlign: TextAlign.center),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Exchange your items for",
+                  style: KPageViewTextStyle2, textAlign: TextAlign.center),
+              SizedBox(
+                height: 20,
+              ),
+              Text("commensurate alternatives.",
+                  style: KPageViewTextStyle2, textAlign: TextAlign.center),
+              SizedBox(height: 100),
+              Container(
+                width: 100,
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  constraints: BoxConstraints.expand( height: 30),
+                  child: (OutlinedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(KProceedColor  ),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                    ),
+                    onPressed: () {
+                      print('skip clicked');
+                      AppNavigator.navigateToWelcomeScreen(context);
+                    },
+                    child: Text(
+                      "Skip",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15),
+                    ),
+                  )),
+                ),
+              ),
+            ],
+          ),
         ),
-        Text("Bring your item and get",
-            style: KPageViewTextStyle2, textAlign: TextAlign.center),
-        SizedBox(
-          height: 20,
-        ),
-        Text("to trade with another trader",
-            style: KPageViewTextStyle2, textAlign: TextAlign.center),
-        SizedBox(height: 20),
-      ],
+      ),
     ),
-    Column(
-      children: [
-        Image.asset("images/onboardscreen-3.png"),
-        SizedBox(height: 100,),
-        Text("Exchange your items for another",
-            style: KPageViewTextStyle, textAlign: TextAlign.center),
-        SizedBox(
-          height: 20,
+    Container(
+      decoration: BoxDecoration(color: Colors.white),
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/onb3.png'),
+                  fit: BoxFit.cover)),
+          child: Column(
+            children: [
+
+              SizedBox(height: 320,),
+              Text("Barter with Ease.",
+                  style: KPageViewTextStyle, textAlign: TextAlign.center),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Login to trade conveniently,",
+                  style: KPageViewTextStyle2, textAlign: TextAlign.center),
+              SizedBox(
+                height: 20,
+              ),
+              Text("fast and secured",
+                  style: KPageViewTextStyle2, textAlign: TextAlign.center),
+              SizedBox(height: 100),
+              Container(
+                width: 200,
+                child: (OutlinedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(KProceedColor  ),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                  ),
+                  onPressed: () {
+                    print('Proceed clicked');
+                    AppNavigator.navigateToWelcomeScreen(context);
+                  },
+                  child: Text(
+                    "Let's Get Started!",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15),
+                  ),
+                )),
+              ),
+            ],
+          ),
         ),
-        Text("Bring your item and get",
-            style: KPageViewTextStyle2, textAlign: TextAlign.center),
-        SizedBox(
-          height: 20,
-        ),
-        Text("to trade with another trader",
-            style: KPageViewTextStyle2, textAlign: TextAlign.center),
-        SizedBox(height: 20),
-      ],
+      ),
     ),
   ];
   return pages;
@@ -88,7 +202,10 @@ class _OnBoardScreenState extends State<OnBoardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
+
       body: SafeArea(
+
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -103,38 +220,25 @@ class _OnBoardScreenState extends State<OnBoardScreen> {
                 },
               ),
             ),
-            new DotsIndicator(
-              dotsCount: pageView(context).length,
-              position: _currentPage.toDouble(),
-              decorator: DotsDecorator(
-                size: const Size.square(9.0),
-                activeSize: const Size(18.0, 9.0),
-                activeShape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0)),
+
+            Container(
+
+              decoration: BoxDecoration(color: Colors.white),
+              alignment: Alignment(0.0, -1.0),
+              child: new DotsIndicator(
+                dotsCount: pageView(context).length,
+                position: _currentPage.toDouble(),
+                decorator: DotsDecorator(
+                  size: const Size.square(9.0),
+                  activeSize: const Size(18.0, 9.0),
+                  activeShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
               ),
             ),
-            Container(
-              width: 200,
-              child: (OutlinedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(KfilterBorderColors ),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0))),
-                ),
-                onPressed: () {
-                  print('Proceed clicked');
-                  AppNavigator.navigateToWelcomeScreen(context);
-                },
-                child: Text(
-                  "Get Started",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 15),
-                ),
-              )),
-            ),
-            SizedBox(height: 20,)
+
+
+
           ],
         ),
       ),
