7bf8f775e0fa9a26f43450b75c98d4937f5e67af
ogomegbunam <Okoriechristian001@gmail.com>
2021-07-06 11:03:17 +0100

    Fashion and antiques
    Fashion and antiques

---
+++ lib/screens/fashion.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class FashionScreen extends StatefulWidget {
+  FashionScreen({Key key}) : super(key: key);
+
+  @override
+  _FashionScreenState createState() => _FashionScreenState();
+}
+
+class _FashionScreenState extends State<FashionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        titleSpacing: 80,
+        backgroundColor: Colors.white60,
+        title: Text(
+          'Fashion',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Fashionitem(
+                label: 'Bags',
+                Onpressed: () {},
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Fashionitem(
+                label: 'Clothings',
+                Onpressed: () {},
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Fashionitem(
+                label: 'Clothing Accessories',
+                Onpressed: () {},
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Fashionitem(
+                label: 'Jewelleries',
+                Onpressed: () {},
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Fashionitem(
+                label: 'Shoes',
+                Onpressed: () {},
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Fashionitem(
+                label: 'Watches',
+                Onpressed: () {},
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
+
+class Fashionitem extends StatelessWidget {
+  const Fashionitem({
+    this.label,
+    this.Onpressed,
+  });
+
+  final String label;
+  final Function Onpressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: Onpressed,
+      child: Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              border: Border.all(
+                color: Color(0xffa60000),
+              )),
+          height: 45,
+          child: Center(
+            child: Text(label),
+          )),
+    );
+  }
+}
--- lib/screens/verificationScreen.dart
+++ lib/screens/verificationScreen.dart
@@ -15,31 +15,32 @@ class _VerifyScreenState extends State<VerifyScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-        body: Container(
-          decoration: BoxDecoration(color: Colors.white),
-          height: MediaQuery.of(context).size.height,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
+      body: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage('images/verificationbg.png'),
                       fit: BoxFit.cover)),
-
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 120,),
-                    Container(
-
-                      child: Center(
-                        child: Column(
-
-                          children: [
-                            Container(
-                              child: Text("Verification Code", style: KPageViewTextStyle,),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 120,
+                  ),
+                  Container(
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Text(
+                              "Verification Code",
+                              style: KPageViewTextStyle,
                             ),
                           ),
                           SizedBox(height: 10),
@@ -104,50 +105,47 @@ class _VerifyScreenState extends State<VerifyScreen> {
                             fontSize: 15),
                       ),
                     ),
-
-                    SizedBox(height:40),
-
-                   Container(
-                     margin: EdgeInsets.all(20),
-                     constraints: BoxConstraints.expand(width: 500, height: 40),
-                     child:OutlinedButton(
-                       style: ButtonStyle(
-                         backgroundColor: MaterialStateProperty.all(KfilterBorderColors ),
-                         shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                             borderRadius: BorderRadius.circular(10.0))),
-                       ),
-                       onPressed: () {
-                         print('verify clicked');
-                         AppNavigator.navigateToWelcomeScreen(context);
-                       },
-                       child: Text(
-                         "Verify",
-                         style: TextStyle(
-                             color:  const Color(0xFFFFFFFF) ,
-                             fontWeight: FontWeight.w500,
-                             fontSize: 15),
-                       ),
-                     ),
-                   ),
-                    SizedBox(height:20),
-
-                    Center(
-                      child: Container(
-                          child: GestureDetector(
-                        child: Text("Click here to resend",
-                            style: TextStyle(
-                              color: const Color(0xFFE0BF0F),
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              decoration: TextDecoration.underline,
-                            )),
-                        onTap: () {},
-                      )),
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    constraints: BoxConstraints.expand(width: 500, height: 40),
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(KfilterBorderColors),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                      onPressed: () {
+                        print('verify clicked');
+                        AppNavigator.navigateToWelcomeScreen(context);
+                      },
+                      child: Text(
+                        "Verify",
+                        style: TextStyle(
+                            color: const Color(0xFFFFFFFF),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15),
+                      ),
                     ),
-
-                SizedBox(height: 100)
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: Container(
+                        child: GestureDetector(
+                      child: Text("Click here to resend",
+                          style: TextStyle(
+                            color: const Color(0xFFE0BF0F),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          )),
+                      onTap: () {},
+                    )),
+                  ),
+                  SizedBox(height: 100)
+                ],
               ),
             ),
           ),
