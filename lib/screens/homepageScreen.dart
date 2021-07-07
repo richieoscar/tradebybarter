d5b842c5261cf84c0a57c208b1a2789cbfc30e74
richieoscar <oscaranyiam94@gmail.com>
2021-07-07 12:44:31 +0100

    popular demand

--- lib/screens/homepageScreen.dart
+++ lib/screens/homepageScreen.dart
@@ -120,11 +120,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(right: 10.0),
-                    child: Text(
-                      'See All',
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        color: Color(0xFFA60000),
+                    child: GestureDetector(
+                      onTap: ()=> AppNavigator.navigateToPopularDemandScreen(context),
+                      child: Text(
+                        'See All',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          color: Color(0xFFA60000),
+                        ),
                       ),
                     ),
                   ),
